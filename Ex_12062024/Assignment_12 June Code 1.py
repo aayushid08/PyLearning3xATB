b6e7e90b24164fffc4dcf7c947043286d344faea
aayushid08 <aayushi.dhondphale7@gmail.com>
2024-06-18 22:42:11 +0530

    Assignment Task

--- Ex_12062024/Assignment_12 June Code 1.py
+++ Ex_12062024/Assignment_12 June Code 1.py
@@ -5,18 +5,39 @@
 # Input = 2024
 # Output = Leap year
 
-year = int(input("Enter a number: "))
+# year = int(input("Enter a number: "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print(f"{year} is leap year")
-        else:
-            print(f"{year} is not a leap year")
-    else:
-        print(f"{year} is leap year")
-else:
-    print(f"{year} is not a leap year")
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(f"{year} is leap year")
+#         else:
+#             print(f"{year} is not a leap year")
+#     else:
+#         print(f"{year} is leap year")
+# else:
+#     print(f"{year} is not a leap year")
 
 
 ########################################
+
+# other way what Pramod explained
+
+year = 2028
+
+(year % 4 == 0)
+(year % 100 != 0)
+(year % 400 == 0)
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("leap year")
+else:
+    print("not a leap year")
+
+
+
+
+
+
+
+
--- Ex_12062024/Assignment_12 June Code 3.py
+++ Ex_12062024/Assignment_12 June Code 3.py
@@ -1,13 +1,54 @@
 # ✅ Task - Fibonacci series and Factorial
 # # 3. Factorial
 # # n = 5
-# # 5! -->5*4*3*2*1 -> 120
+# # 5! -> 5*4*3*2*1 -> 120
 # # 3! -> 3*2*1 -> 6
 # # 4! -> 4*3*2*1 -> 24
 
-fact = int(input("Enter a number: "))
-for i in range(1, fact):
-    fact = fact * i
-print("Factorial is: ", fact)
+import math
 
-########################################
+n = int(input("Enter a number: "))
+fact = 1    # factorial avribale will not be availbe so somewhere we have to store the value
+# result = math.factorial(n)
+# print(result)
+
+
+# using for loop
+#
+# for i in range(1, n+1):     # n+1 isliye liya kyuki agar sirf n daalte to vo value 1 2 3 4 aise leta. agar n+1 dalege to 5 value lega vo chahiye
+#     fact = fact * i
+#
+#
+# print(fact)
+
+
+###############
+# i   F
+# 1   1=1*1
+# 2   2=2*1
+# 3   6=3*2
+# 4   24=4*6
+# 5   120=5*24
+
+#################
+
+while n > 0:
+    fact = fact * n
+    n = n - 1
+print(fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
